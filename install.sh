1ceb7939f75676018780cf8364c1604870200df8
Paull <paull@qq.com>
2016-12-28 11:32:19 +0800

    重复执行安装脚本时不会插入多个入口按钮到互联网页

--- install.sh
+++ install.sh
@@ -2,15 +2,21 @@ NAME=FIFA助手
 DOWNFROM=https://yourdownloadmirror.com/downloads
 FILENAME=yourpackagename.tar.gz
 if [ $(grep 'admin_web.*upgrade' /usr/lib/lua/luci/view/admin_web/home.htm|wc -l) -eq 1 ]; then
-    WHERE="首页"
-    echo ">>>备份首页文件..."
-    [ ! -f /usr/lib/lua/luci/view/admin_web/home.backup ] && cp /usr/lib/lua/luci/view/admin_web/home.htm /usr/lib/lua/luci/view/admin_web/home.backup
-    echo ">>>修改首页按钮..."
-    sed -i 's/admin_web.*upgrade.*%>/admin_web'"'"','"'"'easucks'"'"')%>">'"$NAME"'/' /usr/lib/lua/luci/view/admin_web/home.htm
+    ENTRY="首页"
+    [ ! -f /usr/lib/lua/luci/view/admin_web/home.backup ] && {
+        echo ">>>备份首页文件..."
+        cp /usr/lib/lua/luci/view/admin_web/home.htm /usr/lib/lua/luci/view/admin_web/home.backup
+    }
+    if [ $(grep $NAME /usr/lib/lua/luci/view/admin_web/home.htm|wc -l) -lt 1 ]; then
+        echo ">>>修改首页按钮..."
+        sed -i 's/admin_web.*upgrade.*%>/admin_web'"'"','"'"'easucks'"'"')%>">'"$NAME"'/' /usr/lib/lua/luci/view/admin_web/home.htm
+    fi
 else
-    WHERE="互联网页"
-    echo ">>>备份互联网页文件..."
-    [ ! -f /usr/lib/lua/luci/view/admin_web/network/index.backup ] && cp /usr/lib/lua/luci/view/admin_web/network/index.htm /usr/lib/lua/luci/view/admin_web/network/index.backup
+    ENTRY="互联网页"
+    [ ! -f /usr/lib/lua/luci/view/admin_web/network/index.backup ] && {
+        echo ">>>备份互联网页文件..."
+        cp /usr/lib/lua/luci/view/admin_web/network/index.htm /usr/lib/lua/luci/view/admin_web/network/index.backup
+    }
     if [ $(grep $NAME /usr/lib/lua/luci/view/admin_web/network/index.htm|wc -l) -lt 1 ]; then
         echo ">>>在互联网页插入按钮..."
         sed -i 's/<ul id="advanced_setup">/<ul id="advanced_setup">\n<li><a href="<%=luci.dispatcher.build_url('"'"'admin_web'"'"','"'"'easucks'"'"')%>">'"$NAME"'<\/a><\/li>/' /usr/lib/lua/luci/view/admin_web/network/index.htm
@@ -30,7 +36,7 @@ rm -f /usr/bin/ss-tunnel
 rm -f /tmp/$FILENAME
 echo ">>>安装完成！"
 if [ -f /var/run/luci-indexcache ]; then
-    rm /var/run/luci-indexcache && echo ">>>请到极路由后台[$WHERE]找[$NAME]按钮进入插件"
+    rm /var/run/luci-indexcache && echo ">>>请到极路由后台[$ENTRY]找[$NAME]按钮进入插件"
 else
-    echo ">>>需要手动重启路由器，重启后到极路由后台[$WHERE]找[$NAME]按钮进入插件"
+    echo ">>>需要手动重启路由器，重启后到极路由后台[$ENTRY]找[$NAME]按钮进入插件"
 fi
